fb484d76e3328ff21e9d0fbf9ab1aa9cc93a35f6
gamari <gamari4552@gmail.com>
2023-09-15 14:16:30 +0900

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -8,11 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    "*"
-]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default="").split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
